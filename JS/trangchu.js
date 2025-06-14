805c220fd697f953b8d39d5c074311323ef5c979
your name <huynguyen.2764@gmail.com>
2025-06-14 18:22:44 +0700

    update

---
+++ JS/trangchu.js
@@ -0,0 +1,19 @@
+// tao hieu ung khi cuon toi trang chu
+const allSections = document.querySelectorAll(".home__div--hidden, .home__img--hidden, .home__section--hidden");
+const revealSection = function(entries, observer){
+    const [entry] = entries;
+    if(!entry.isIntersecting) return;
+    entry.target.classList.remove("home__div--hidden");
+    entry.target.classList.remove("home__img--hidden");
+    entry.target.classList.remove("home__section--hidden");
+    observer.unobserve(entry.target);
+};
+
+    const sectionObserver = new IntersectionObserver(revealSection, {
+        root: null,
+        threshold: 0
+    });
+
+allSections.forEach(function(section){
+    sectionObserver.observe(section);
+});
