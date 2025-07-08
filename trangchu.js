667673098e7d8fcc38cbdaff3dbc7080984e135e
your name <huynguyen.2764@gmail.com>
2025-07-08 11:03:03 +0700

    update

--- trangchu.js
+++ trangchu.js
@@ -1,3 +1,4 @@
+// Nguyen Van Hoang Huy - B2308362
 // tao hieu ung khi cuon toi trang chu
 const allSections = document.querySelectorAll(".home__div--hidden, .home__img--hidden, .home__section--hidden");
 const revealSection = function(entries, observer){
