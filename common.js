af67e287d53b496c5d58778a29f6c994d187a1f6
your name <huynguyen.2764@gmail.com>
2025-07-11 04:45:08 +0700

    update

---
+++ common.js
@@ -0,0 +1,96 @@
+const buttonAcc = document.getElementById("buttonAcc");
+const buttonSignin = document.getElementById("buttonSignin");
+const accountContent = document.getElementById("accountContent");
+const buttonSignout = document.getElementById("buttonSignout");
+const textTitle = document.getElementById("result1");
+const textDesc = document.getElementById("result2");
+const user = localStorage.getItem("currentUser");
+const cartButton = document.getElementById("cartButton");
+
+// Hiển thị ngày giờ hiện tại
+function greetingTime() {
+    const today = new Date();
+    const hours = today.getHours();
+    let greeting;   
+    if (hours >= 5 && hours < 12) {
+        greeting = "Good morning";
+    } else if (hours >= 12 && hours < 18) {
+        greeting = "Good afternoon";
+    } else if (hours >= 18 && hours < 22) {
+        greeting = "Good evening";
+    } else {
+        greeting = "Good night";
+    }
+    return greeting;
+}
+
+
+const gtMessage = greetingTime();
+const today = new Date();
+const date = today.getDate();
+const month = today.getMonth() + 1;
+const year = today.getFullYear();
+const week = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+const dayofweek = today.getDay();
+
+buttonAcc.addEventListener("click", () =>{
+    if(accountContent.classList.contains("none")){
+        accountContent.classList.remove("none");
+        accountContent.classList.add("block");
+    }
+    else{
+        accountContent.classList.remove("block");
+        accountContent.classList.add("none");
+    }
+});
+
+// check đăng nhaapj
+
+function isLogIn() {
+    return localStorage.getItem("currentUser") !== null;
+}
+
+function checkLogin() {
+  if (isLogIn()) {
+    buttonSignin.style.display = "none";
+    buttonAcc.style.display = "flex";
+  } else {
+    buttonSignin.style.display = "flex";
+    buttonAcc.style.display = "none";
+  }
+}
+checkLogin();
+
+// dang xuat
+buttonSignout.addEventListener("click", () => {
+    localStorage.removeItem("currentUser");
+    window.location.href = "../trangchu.html";
+})
+
+//Hienthi thong bao cua tai khoan
+function innerName() {
+    textTitle.innerHTML = `Xin chào, ${user}`;
+    textDesc.innerHTML = `${gtMessage} today is ${week[dayofweek]}, ${date}/${month}/${year}`;
+}
+innerName();
+
+function moveToCart() {
+  window.location.href = "./cart/giohang.html";
+}
+
+// check neu ch dang nhap thi ko cho vao cart
+cartButton.addEventListener("click", (event) => {
+  if (!isLogIn()) {
+    event.preventDefault();
+    alert("Vui lòng đăng nhập để xem giỏ hàng");
+    return;
+  } else moveToCart();
+});
